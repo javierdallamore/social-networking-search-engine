814d3b4d95b14dae368fc65ec6654e80c09d80a3
Javier Dall' Amore <javierdallamore@gmail.com>
2011-12-06 01:47:25 -0300

    js de busqueda armado como corresponde

--- SocialNetworkingSearchEngine/SocialNetWorkingSearchEngine/Scripts/search.js
+++ SocialNetworkingSearchEngine/SocialNetWorkingSearchEngine/Scripts/search.js
@@ -1,7 +1,19 @@
-function search() {
-    $.getJSON("SearchResults", { parameters: $("#txtParameters").val(), searchEngines: $("#txtSearchEngines").val() }, function (json) {
-        var result = "";
-        _.each(json[0].SocialNetworkingItems, function (item) { result += item.Content + "\n"; });
-        $("#txtResult").val(result);
-    });
-};
+$(document).ready(function() {
+    $("#tuboton").click(onClick);
+
+    function onClick(e) {
+        var valoresDeCheckbox = $("#txtSearchEngines").val();
+        $.getJSON("SearchResults", { parameters: $("#txtParameters").val(), searchEngines: valoresDeCheckbox }, function (json) {
+            _.each(json, function (socialNetworkingSearchResult) {
+                // mostras el socialNetworkingSearchResult.SocialNetworkingName
+                _.each(socialNetworkingSearchResult.SocialNetworkingItems, function (socialNetworkingItems) {
+                    // metes un link al socialNetworkingItems.Id
+                    // mostras el socialNetworkingItems.StatusDate
+                    // mostras el socialNetworkingItems.Content
+                    // mostras el socialNetworkingItems.ProfileImage
+                    // mostras el socialNetworkingItems.UserName
+                    });
+                });
+            });
+        };
+});
