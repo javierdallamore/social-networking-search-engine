f10058db645df4377b338111371f5e7d83496c50
Javier Dall' Amore <javierdallamore@gmail.com>
2011-12-05 23:23:13 -0300

    Creacion de todos los servicios extras como taguear o guardar profile u obtener todo

--- SocialNetworkingSearchEngine/SocialNetWorkingSearchEngine/Controllers/HomeController.cs
+++ SocialNetworkingSearchEngine/SocialNetWorkingSearchEngine/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessRules;
+using Core.Domain;
 using SearchEnginesBase.Entities;
 using SocialNetWorkingSearchEngine.Models;
 
@@ -43,5 +44,58 @@ namespace SocialNetWorkingSearchEngine.Controllers
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult GetAllEntities(string parameters, string searchEngines)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.GetAllEntities(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAllEntitiesByProfile(string profileId)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.GetAllEntitiesByProfile(Guid.Parse(profileId)), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAllEntitiesByTag(string tagName)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.GetAllEntitiesByTag(tagName), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAllProfiles()
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.GetAllProfiles(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetAllTags()
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.GetAllTags(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveEntity(Entity entity)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.SaveEntity(entity), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveEntity(Profile profile)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.SaveProfile(profile), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveEntity(Tag tag)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.SaveTag(tag), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult TagEntity(Entity entity, string tagName)
+        {
+            var servicesManager = new ServicesManager();
+            return Json(servicesManager.TagEntity(entity, tagName), JsonRequestBehavior.AllowGet);
+        }
     }
 }
