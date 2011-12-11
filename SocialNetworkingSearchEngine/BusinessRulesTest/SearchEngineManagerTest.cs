d8f9626be14d36ab4d9d950b4feaca6ad7efbda2
Gabriel Mamani <rgmamani@gmail.com>
2011-12-11 19:58:00 -0300

    Cambios en la configuracion y en los mapeos

--- SocialNetworkingSearchEngine/BusinessRulesTest/SearchEngineManagerTest.cs
+++ SocialNetworkingSearchEngine/BusinessRulesTest/SearchEngineManagerTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using BusinessRules;
 using DataAccess;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
