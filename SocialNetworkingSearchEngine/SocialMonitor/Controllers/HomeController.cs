cd4e301fecb8c5ab948a9d2d5579ed7a999ba5a1
Marcos Mellibovsky <mellibo@gmail.com>
2012-10-02 19:11:17 -0300

    proyecto SocialMonitor con la API

---
+++ SocialNetworkingSearchEngine/SocialMonitor/Controllers/HomeController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialMonitor.Controllers
+{
+    using System.Collections.ObjectModel;
+    using System.Web.Helpers;
+
+    using BusinessRules;
+
+    using Core.Domain;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ProcesarPosts(ProcesarPostModel procesarPostModel)
+        {
+            return Json(new { success = 0 });
+        }
+
+        public JsonResult ProcesarPostsModelSample1()
+        {
+            return
+                Json(
+                    new ProcesarPostModel
+                        {
+                            PostIds = new int[] { 1, 2, 3 },
+                            Tags =
+                                new Collection<TagViewModel>()
+                                    {
+                                        new TagViewModel { Id = 1, Name = "Insulto" },
+                                        new TagViewModel { Name = "New tag" }
+                                    },
+                            Sentiment = SentimentEnum.Positivo
+                        },
+                    JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ProcesarPostsReturnedJsonOnSuccess()
+        {
+            return Json(new { success = 0 }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ProcesarPostsReturnedJsonOnFailure()
+        {
+            return Json(new { success = 1, message = "Algo esta mal" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetTags()
+        {
+            return
+                Json(
+                    new Collection<TagViewModel>
+                        {
+                            new TagViewModel() { Id = 1, Name = "Tag 1" },
+                            new TagViewModel() { Id = 2, Name = "Tag 2" },
+                            new TagViewModel() { Id = 3, Name = "Tag 3" },
+                            new TagViewModel() { Id = 4, Name = "Tag 4" }
+                        },
+                    JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetPostsModelSample1()
+        {
+            return
+                Json(
+                    new PostsFilterModel
+                        {
+                            Desde = DateTime.Today.AddDays(-10).ToString("dd/MM/yyyy"),
+                            Hasta = DateTime.Today.ToString("dd/MM/yyyy"),
+                            Action = "Process",
+                            Engines = new int[] { 1, 2, 3 },
+                            NumPage = 1,
+                            PageSize = 10
+                        },
+                    JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class PostsFilterModel
+    {
+        public string Desde { get; set; }
+
+        public string Hasta { get; set; }
+
+        public IEnumerable<int> Tags { get; set; }
+
+        public IEnumerable<int> Engines { get; set; }
+
+        public int? Folder { get; set; }
+
+        public bool FolderRecursive { get; set; }
+
+        public IEnumerable<int> Sentiments { get; set; }
+
+        public int? State { get; set; }
+
+        public int NumPage { get; set; }
+
+        public int PageSize { get; set; }
+
+        public string Action { get; set; }
+
+        public int? Query { get; set; }
+    }
+
+    public class TagViewModel
+    {
+        public int? Id { get; set; }
+        
+        public string Name { get; set; }
+    } 
+
+    public class ProcesarPostModel
+    {
+        public IEnumerable<TagViewModel> Tags { get; set; }
+        
+        public IEnumerable<int> PostIds { get; set; }
+        
+        public SentimentEnum? Sentiment { get; set; }
+    }
+
+    public enum SentimentEnum
+    {
+        Positivo = 1,
+
+        Negativo = 2,
+
+        Neutro = 0
+    }
+}
--- SocialNetworkingSearchEngine/SocialNetWorkingSearchEngine/Controllers/PostManagerController.cs
+++ SocialNetworkingSearchEngine/SocialNetWorkingSearchEngine/Controllers/PostManagerController.cs
@@ -7,40 +7,53 @@ using SocialNetWorkingSearchEngine.Models;
 
 namespace SocialNetWorkingSearchEngine.Controllers
 {
+    using Core.Domain;
+
     [Authorize]
     public class PostManagerController : Controller
     {
+        private readonly ServicesManager servicesManager = new ServicesManager();
+
         public ActionResult Index()
         {
-            var servicesManager = new ServicesManager();
             var userHomeModel = new PostManagerModel();
-            
-            userHomeModel.NegativeWords = servicesManager.GetAllWords().Where(x=>x.Sentiment.ToLower() == "negativo");
+
+            userHomeModel.NegativeWords = servicesManager.GetAllWords().Where(x => x.Sentiment.ToLower() == "negativo");
             userHomeModel.PositiveWords = servicesManager.GetAllWords().Where(x => x.Sentiment.ToLower() == "positivo");
 
-            var userAssignedPostNotProcessed =
-                servicesManager.GetNotProcessedUserAssignedPost(UserHelper.GetCurrent()).ToList();
-            var diferenceInPostToProcess = Params.MaxPostToProcessPerUser - userAssignedPostNotProcessed.Count();
-            
-            if (diferenceInPostToProcess > 0)
-            {
-                var newAssignedPost = servicesManager.AssignPostToUser(diferenceInPostToProcess, UserHelper.GetCurrent());
-                userAssignedPostNotProcessed = userAssignedPostNotProcessed.Concat(newAssignedPost).ToList();
-            }
+            var userAssignedPostNotProcessed = GetAndAssignPosts();
             userHomeModel.Posts = userAssignedPostNotProcessed;
 
 
             return View(userHomeModel);
         }
 
+        public JsonResult GetPosts()
+        {
+            return Json(this.GetAndAssignPosts(), JsonRequestBehavior.AllowGet);
+        }
+
         [HttpPost]
         public JsonResult UpdatePost(string idPost, int rating, string sentiment, List<string> tags)
         {
-            var servicesManager = new ServicesManager();
             var currentUser = UserHelper.GetCurrent();
             servicesManager.UpdatePost(idPost, rating, sentiment, tags, currentUser);
 
             return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        private IEnumerable<Post> GetAndAssignPosts()
+        {
+            var userAssignedPostNotProcessed = servicesManager.GetNotProcessedUserAssignedPost(UserHelper.GetCurrent()).ToList();
+            var diferenceInPostToProcess = Params.MaxPostToProcessPerUser - userAssignedPostNotProcessed.Count();
+
+            if (diferenceInPostToProcess > 0)
+            {
+                var newAssignedPost = servicesManager.AssignPostToUser(diferenceInPostToProcess, UserHelper.GetCurrent());
+                userAssignedPostNotProcessed = userAssignedPostNotProcessed.Concat(newAssignedPost).ToList();
+            }
+
+            return userAssignedPostNotProcessed;
         }
     }
 }
